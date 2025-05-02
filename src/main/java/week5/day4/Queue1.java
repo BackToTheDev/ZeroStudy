e520a02fa25881fba6948329e4a67a9610580ee5
BackToTheDev <rnjswnstjd13@gmail.com>
2025-05-02 16:22:39 +0900

    Queue

---
+++ src/main/java/week5/day4/Queue1.java
@@ -0,0 +1,63 @@
+package week5.day4;
+import java.util.*;
+import java.io.*;
+
+public class Queue1 {
+
+    public static class Queue {
+        private int[] arr;
+        private int front, rear, size;
+
+        public Queue() {
+            this.arr = new int[10];
+            front = 0;
+            rear = -1;
+            size = 0;
+        }
+
+        public void enqueue(int value) {
+            if (size == arr.length) return;
+            this.arr[(++rear % arr.length)] = value;
+            size++;
+        }
+
+        public void dequeue() {
+            if (size == 0) return;
+            front++;
+            size--;
+        }
+
+        public int peek() {
+            return arr[front % arr.length];
+        }
+
+        public void printQueue() {
+            StringBuffer sb = new StringBuffer();
+            for (int data : arr) {
+                sb.append(data).append(" ");
+            }
+            System.out.println(sb.toString());
+        }
+
+    }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Queue queue = new Queue();
+        for (int i = 1; i <= 10; i++) {
+            queue.enqueue(10 * i);
+        }
+        queue.printQueue();
+        queue.enqueue(200);
+        queue.printQueue();
+
+        System.out.println(queue.peek());
+
+
+        for (int i = 1; i <= 10; i++) {
+            queue.dequeue();
+            System.out.println(queue.peek());
+            queue.printQueue();
+        }
+    }
+}
