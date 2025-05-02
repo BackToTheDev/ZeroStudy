f4eb7a87d0e758bef2dd1622b5ce5af0d084e4a2
BackToTheDev <rnjswnstjd13@gmail.com>
2025-05-02 20:24:18 +0900

    Queue5

--- src/main/java/week5/day4/Queue5.java
+++ src/main/java/week5/day4/Queue5.java
@@ -1,4 +1,52 @@
 package week5.day4;
+import java.util.*;
+import java.io.*;
 
 public class Queue5 {
+    public static class Queue {
+        private LinkedList<Integer> list;
+
+
+        public Queue() {
+            list = new LinkedList<>();
+        }
+
+        public void enqueue(int value) {
+            list.addLast(value);
+        }
+
+        public int dequeue() {
+            if (list.isEmpty()) {
+                System.out.println("큐가 비어있습니다.");
+                return -1;
+            }
+            return list.removeFirst();
+        }
+
+        public int peek() {
+            if (list.isEmpty()) {
+                System.out.println("큐가 비어있습니다.");
+                return -1;
+            }
+            return list.getFirst(); // list.get(0);
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue4.Queue queue = new Queue4.Queue();
+
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+
+        System.out.println(queue.dequeue()); // 1 (제거되면서)
+        System.out.println(queue.peek()); // 2 (확인만)
+        queue.enqueue(4);
+        System.out.println(queue.dequeue()); // 2 (제거)
+        System.out.println(queue.dequeue()); //3
+        System.out.println(queue.dequeue()); // 4
+        System.out.println(queue.dequeue());
+    }
 }
+
+
